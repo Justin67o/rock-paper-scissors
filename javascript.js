a929117d5d97793dd7079fd37098ae53650a5827
Justin67o <jg3chen@uwaterloo.ca>
2025-08-06 12:16:03 -0400

    Added function to play round and run the game
    playRound compares the values of getHumanChoice and
    getComputerChoice and sees which should win, and
    returns the winner.
    playGame runs playRound 5 times and tracks the number
    of wins for human or computer

--- javascript.js
+++ javascript.js
@@ -1,7 +1,4 @@
-//create variable to track player score
-let playerScore = 0;
-//create variable to track computer score
-let computerScore = 0;
+
 
 //FOR 5 times
     //Prompt user for rock, paper, or scissors
@@ -11,11 +8,11 @@ function getComputerChoice(){
         //generate random number from 1-3
         // 1 = rock, 2 = paper, 3 = scissors
     if (result === 1){
-        return "Rock";
+        return "rock";
     } else if (result === 2){
-        return "Paper";
+        return "paper";
     } else{
-        return "Scissors";
+        return "scissors";
     }
 }
 
@@ -23,10 +20,52 @@ function getHumanChoice(){
     let result = prompt("Rock, Paper, or Scissors?");
     return result;
 }
-console.log(getHumanChoice());
 
-        
-    //IF player wins
-        //increase player score
-    //else 
-        // increase computer score
+
+function playRound(humanChoice, computerChoice){
+    //player loses
+    console.log(humanChoice);
+    console.log(computerChoice);
+    if (humanChoice.toLowerCase() == "rock" && computerChoice == "paper"){
+        console.log("You lose! Paper beats Rock.");
+        return "Computer";
+    } else if(humanChoice.toLowerCase() == "paper" && computerChoice == "scissors"){
+        console.log("You lose! Scissors beats Paper!");
+        return "Computer";
+    } else if(humanChoice.toLowerCase() == "scissors" && computerChoice == "rock"){
+        console.log("You lose! Rock beats Scissors!");
+        return "Computer";
+    //player wins
+    } else if(humanChoice.toLowerCase() == "rock" && computerChoice == "scissors"){
+        console.log("You win! Rock beats Scissors!");
+        return "Player";
+    } else if(humanChoice.toLowerCase() == "paper" && computerChoice == "rock"){
+        console.log("You win! Paper beats Rock!");
+        return "Player";
+    } else if(humanChoice.toLowerCase() == "scissors" && computerChoice == "paper"){
+        console.log("You win! Scissors beats paper!");
+        return "Player";
+    //player ties
+    } else if(humanChoice.toLowerCase() == computerChoice){
+        console.log("You win! " + humanChoice + " ties with " + computerChoice + "!");
+    }
+}
+
+
+
+function playGame(){
+    //create variable to track player score
+    let playerScore = 0;
+    //create variable to track computer score
+    let computerScore = 0;
+    for (i = 0; i < 5; i++){
+        let result = playRound(getHumanChoice(), getComputerChoice());
+        if (result == "Player"){
+            playerScore++;
+        } else{
+            computerScore++;
+        }
+    }
+}
+
+playGame();
